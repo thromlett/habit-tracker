6382a1077a6e3fd65e21499aaf212003901265d1
DEADMEN5736 <116201158+DEADMEN5736@users.noreply.github.com>
2025-05-16 15:44:01 -0500

    test trigger CI workflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 export default function HomePage() {
     return (
-          <a href="/test-runner">She h on my ref til I page.tsx</a>
+          <a href="/test-runner">She h on my ref til I page.tsx now im package.json</a>
     )
   }
-  
