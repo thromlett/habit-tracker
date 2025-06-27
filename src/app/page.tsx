e73e275bb94eaa4f00ce9cb24d970e5d50cbf156
DEADMEN5736 <116201158+DEADMEN5736@users.noreply.github.com>
2025-06-27 16:43:21 -0500

    Redirects from blank homepage to dashboard upon login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/authOptions";
 import { redirect } from "next/navigation";
-import HomeClient from "./components/HomeClient";
 
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
@@ -9,6 +8,5 @@ export default async function HomePage() {
   if (!session) {
     redirect("/login");
   }
-
-  return <HomeClient user={session.user} />;
+  redirect("/dashboard");
 }
