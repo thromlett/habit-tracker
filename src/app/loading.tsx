1030e9a325fb748d53a47e4bd15c5559df3e9815
DEADMEN5736 <116201158+DEADMEN5736@users.noreply.github.com>
2025-08-13 16:39:26 -0500

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,9 @@
+import LoadIcon from "@/components/LoadIcon";
+
 export default function RootLoading() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-white/80 z-50">
-      AAAAAAAHHHHHHHH
+      <LoadIcon />
     </div>
   );
 }
